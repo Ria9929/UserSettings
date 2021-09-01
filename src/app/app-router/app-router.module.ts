1816266e50117fd2d5b2b3a77cdac01ee0bdb218
Ria9929 <ria291199@gmail.com>
2021-09-01 15:26:32 +0530

    2 files modified, 1 added

--- src/app/app-router/app-router.module.ts
+++ src/app/app-router/app-router.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {Routes} from '@angular/router';
+import {EditComponent} from'../edit/edit.component';
 const routes: Routes = [{ path: 'edit', component: EditComponent }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
-import { AppRouterModule } from './app-router';
+import {AppRouterModule} from './app-router/app-router.module'
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
